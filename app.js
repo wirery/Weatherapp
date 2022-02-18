d4f3dafba37e46302fdb7886a2e0385722f48f21
wirery <wire_ryan@yahoo.com>
2022-02-18 15:01:29 -0500

    final commit, weather displaying

--- app.js
+++ app.js
@@ -5,17 +5,37 @@ let weather = {
             "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units&appid=" + this.apiKey
         )
         .then((response) => response.json())
-        .then((data) => console.log (data));
+        .then((data) => this.displayWeather (data));
     },
     displayWeather: function(data) {
         const {name} = data;
-        const {icon, description} = data.weather;
+        const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
         console.log (name, icon, description, temp, humidity, speed)
-        document.querySelector('city').innerText = "Weather in" + name;
-        document.querySelector(".icon").ATTRIBUTE_NODE.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-        
+        document.querySelector('.city').innerText = "Weather in" + name;
+       document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed:" + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading")
 
+
+
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
+
+document.querySelector(".search button").addEventListener('click', function() {
+weather.search();
+});
+document.querySelector(".search-bar").addEventListener('keyup', function (event) {
+    if (event.key ** "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Denver");
